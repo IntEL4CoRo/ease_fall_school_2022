ceb6a47292815ebec2c3c7ced0d8e6337d8ce877
Michael Neumann <michael_neumann@gmx.net>
2022-09-01 12:45:01 +0200

    add Day2

---
+++ Day2/launch/cram_launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source /home/workspace/ros/devel/setup.bash
+roslaunch cram_pick_place_tutorial world.launch --wait &
+export THIS_IP=$(ifconfig 'docker0' | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
+sleep 2
+echo ""
+echo ""
+jupyter-lab --allow-root --no-browser --port 8888 --ip=$THIS_IP
