6816ecfeb2f6b1cd21ed205d8424871859a5a5c6
Michael Neumann <michael_neumann@gmx.net>
2022-09-06 17:02:04 +0200

    update Day2

--- Day2/launch/cram_launch.sh
+++ Day2/launch/cram_launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /home/workspace/ros/devel/setup.bash
-roslaunch cram_pick_place_tutorial world.launch --wait &
+roslaunch cram_pr2_pick_place_demo sandbox.launch --wait &
 export THIS_IP=$(ifconfig 'docker0' | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 sleep 2
 echo ""
